c5a332fe3c08caa3dd4f4adb561b0c9a9ad10861
CAlexander0614 <53710031+CAlexander0614@users.noreply.github.com>
2019-10-17 11:41:35 -0400

    Update Athlete.cpp

--- Athlete.cpp
+++ Athlete.cpp
@@ -1,10 +1,3 @@
-//
-//  Athlete.cpp
-//  Flex_Project_Alexander_Chris
-//
-//  Created by Chris Alexander on 3/30/19.
-//  Copyright © 2019 Chris Alexander. All rights reserved.
-//
 
 #include <stdio.h>
 #include "Athlete.h"
